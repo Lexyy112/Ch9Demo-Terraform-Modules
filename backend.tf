b3d9f22b1025defc94d7aa681d9036caa3100ede
Sunday Abioye <lexyy11@live.co.uk>
2021-11-25 11:58:41 +0000

    enter

--- backend.tf
+++ backend.tf
@@ -1,4 +1,8 @@
 terraform {
   backend "azurerm" {
+    resource_group_name      = "rg-uks-Sundayabioye"
+    storage_account_name     = "sundaystore123"
+    container_name           = "terraform"
+    key                      = "terraform.tfstate"
   }
 }
